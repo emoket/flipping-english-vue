4a365b21cbca21965182688e3ed990fa0399d838
Yongjin Park <insight.emoket@gmail.com>
2018-01-11 00:33:47 +0900

    project init

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      fixed
+      :mini-variant="miniVariant"
+      v-model="drawer"
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon" to="item.link"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+
+
+    <v-toolbar fixed app dark class="teal">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="brandName"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>add</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>list</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <main>
+      <router-view></router-view>
+    </main>
+
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        brandName: 'Flipping English',
+        drawer: false,
+        miniVariant: true,
+        items: [
+          { icon: 'add', title: 'Add Expression', link: '/expr/add' },
+          { icon: 'list', title: 'List', link: 'expr/list' },
+          { icon: 'search', title: 'Search', link: 'expr/search' }
+        ]
+      }
+    }
+  }
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-layout column>
+      <v-flex xs12 sm6 offset-sm3>
+        List Expression
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
