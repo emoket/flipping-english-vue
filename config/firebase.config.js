67f559617f2774126ba9f6a4bd0b0e72746103b4
Yongjin Park <insight.emoket@gmail.com>
2018-01-15 23:17:57 +0900

    firebase config

---
+++ config/firebase.config.js
@@ -0,0 +1,9 @@
+'use strict'
+module.exports = {
+  apiKey: "AIzaSyBKJwbGBlYv4PRO9u2feDKAQ_KxOIvNvtg",
+  authDomain: "flipping-english-vue-firebase.firebaseapp.com",
+  databaseURL: "https://flipping-english-vue-firebase.firebaseio.com",
+  projectId: "flipping-english-vue-firebase",
+  storageBucket: "",
+  messagingSenderId: "740525490839"
+}
